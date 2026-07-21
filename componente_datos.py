e8c3fc1d6e6101fd61618064f6bc00d70bf188cb
Geraldin Triana <geraldinprogramacion@gmail.com>
2026-07-21 17:38:59 +0200

    Guardar cambios en componente_datos

--- componente_datos.py
+++ componente_datos.py
@@ -7,12 +7,13 @@ class IngestadorDatos:
         pass
     
     def cargar_datos(self, archivo) -> pd.DataFrame:
-        """ Carga un archivo y valida qiue con la interfaz  requerida """
+        """ Carga un archivo y valida  con la interfaz  requerida """
+
         #El bloque Try,se usa junto con except para manejar errores
         #  y execpciones de forma controlada , evitando que el codigo
         #se rompoa abruptamente al suceder un error, definido
         # acciones alternativas.
-        try: 
+        try:  
             df = pd.read_csv(archivo, sep=None, engine='python', encoding='utf-8-sig')
 
             #  Usamos sep=None y el motor de python para que pandas detecte
@@ -20,7 +21,7 @@ class IngestadorDatos:
 
             #validamos el contrato de la interfaz(columnas requeridas)
             columnas_requeridas= {'fecha','producto','cantidad','precio_unitario'}
-            if not columnas_requeridas.issubset(df.columns): #si columnas requeridas no es algo logico
+            if not columnas_requeridas.issubset(df.columns): #si columnas requeridas no escribe lo anterior requerido.
                 raise ValueError(f"El archivo no cumple con el contrato.columnas requeridas:{ columnas_requeridas}")#me permite mostrar un mensaje de error controlado.
             #limpieza conversión de tipos de datos
             df ['fecha']=pd.to_datetime(df['fecha']) #convertir a un tipo de dato datetime
@@ -28,4 +29,5 @@ class IngestadorDatos:
             return df
         except Exception as e:
             raise IOError(f"Error al procesar el componente de datos(e)")
+        
     
